0f2e405890b8cba312a5cd5f09306a5836c8511f
Nick Vernij <hello@nickforall.nl>
2018-11-06 00:37:30 +0100

    Working prototypye of atoi

--- numberparser.asm
+++ numberparser.asm
@@ -7,28 +7,82 @@ _main:
     push rbp
     mov rbp, rsp
     
+    push 0 ; rbp 
+    push 4 ; rbp + 8
+    
+    ; "471"
     push "1"
     push "7"
     push "4"    
 
-    ; realign stack
-    sub rsp, 16
-    
-    mov rax, 0
-    mov rcx, 0
+    mov rbx, 0 ; tmp
 
     .start:
-    add rcx, 1
-    
-    
+    ; decrease loop count from storage, if 0 jump to end
+    mov rcx, [rbp-16]
+    dec rcx
+    jz .end 
 
-    ; if the accumalator (rcx) < 4, jmp to start to loop again
-    cmp rcx, 4
-    jb .start
+    ; move the loop count back to storage
+    mov [rbp-16], rcx 
     
+    ; 10^(i-1) multiplier
+    mov rdi, rcx
+    dec rdi    
+    call pow
+    
+    ; 0x30 = "0", so every ascii character after that is increasing by 1 
+    pop rbx
+    sub rbx, 0x30
+    
+    ; multiply the position's power by the number
+    imul rax, rbx
+    
+    ; add the multiplier to storage
+    mov rdx, [rbp-8]
+    add rdx, rax
+    
+    ; move rdx back to the storage
+    mov [rbp-8], rdx
+    
+    jmp  .start
+    .end:
+   
     ; exit(0)
-    mov rdi, 0
+    mov rdi, [rbp-8]
     call exit
+
+; gets the power of 10 for a given amount of times
+; rdi is power amount  
+pow:
+    push rbp
+    mov rbp, rsp
+  
+    mov rax, 1 ; acc
+    
+    ; if rdi is zero, return 1 and skip power stuff
+    cmp rdi, 0
+    je .end
+
+    ; if rdi isn't zero, we want to return a power of 10
+    mov rax, 10    
+
+    ; start loop   
+    .start:
+    dec rdi
+    jz .end ; if rdi is zero, go to .end
+    
+    ; multiply rax by 10
+    imul rax, 10
+    
+    ; loop back
+    jmp .start
+    .end:
+    
+    mov rsp, rbp
+    pop rbp
+
+    ret
 
 exit:
     mov rax, 0x2000001 ; exit syscall
